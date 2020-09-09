f296a1318f5d369bb2d84729eb27597fbd1c40be
ryan clemente <kojiee@gmail.com>
2020-09-10 06:24:17 +0800

    add region and zone option

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,11 @@ if [ ! -d "$HOME/.config/gcloud" ]; then
       exit 1
    fi
 
+   if [ -z "${REGION-}" && -z "${ZONE_NAME-}" ]; then
+      echo "REGION and/or ZONE_NAME not found. Exiting...."
+      exit 1
+   fi
+
    echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
 
    gcloud auth activate-service-account --key-file=/tmp/account.json --project "$PROJECT_ID"
@@ -23,7 +28,12 @@ echo ::add-path::/google-cloud-sdk/bin/gcloud
 echo ::add-path::/google-cloud-sdk/bin/gsutil
 
 # Update kubeConfig.
-gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$ZONE_NAME" --project "$PROJECT_ID"
+LOCATION=""
+if [ -z "${REGION-}"]; then LOCATION="$LOCATION --region $REGION"; fi
+if [ -z "${ZONE_NAME-}"]; then LOCATION="$LOCATION --zone $ZONE_NAME"; fi
+
+gcloud container clusters get-credentials "$CLUSTER_NAME" --project "$PROJECT_ID" $LOCATION
+
 
 # verify kube-context
 kubectl config current-context
