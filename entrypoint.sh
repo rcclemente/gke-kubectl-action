8b08e52247ade34c92f2c4e807af975998066f14
ryan clemente <kojiee@gmail.com>
2020-09-10 07:42:43 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ if [ ! -d "$HOME/.config/gcloud" ]; then
       exit 1
    fi
 
-   if [ -z "${REGION-}" && -z "${ZONE_NAME-}" ]; then
+   if [[ -z "${REGION-}"]  && [ -z "${ZONE_NAME-}" ]]; then
       echo "REGION and/or ZONE_NAME not found. Exiting...."
       exit 1
    fi
@@ -29,8 +29,8 @@ echo ::add-path::/google-cloud-sdk/bin/gsutil
 
 # Update kubeConfig.
 LOCATION=""
-if [[ -z "${REGION-}"]]; then LOCATION="$LOCATION --region $REGION"; fi
-if [[ -z "${ZONE_NAME-}"]]; then LOCATION="$LOCATION --zone $ZONE_NAME"; fi
+if [[ ! -z "${REGION-}" ]]; then LOCATION="$LOCATION --region $REGION"; fi
+if [[ ! -z "${ZONE_NAME-}" ]]; then LOCATION="$LOCATION --zone $ZONE_NAME"; fi
 
 gcloud container clusters get-credentials "$CLUSTER_NAME" --project "$PROJECT_ID" $LOCATION
 
